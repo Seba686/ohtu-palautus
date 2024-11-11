d13228cb1856a01faed283208b5a0ddb301d7bf9
Sebastian Beijar <sebastian.beijar@helsinki.fi>
2024-11-11 10:09:14 +0200

    changes

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -4,12 +4,11 @@ from logger import logger
 from summa import summa
 from erotus import erotus
 
-logger("aloitetaan ohjelma")
+logger("aloitetaan")
 
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
 print(f"{summa(x, y)}")
 print(f"{erotus(x, y)}")
 
-logger("lopetetaan ohjelma")
-print("goodbye!") # lisäys bugikorjaus-branchissa
+logger("lopetetaan")
